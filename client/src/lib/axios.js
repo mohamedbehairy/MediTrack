a6d1a15a4be43ec842fb302197cd38e831bd9357
Mohamed Behairy <134211075+mohamedbehairy@users.noreply.github.com>
2026-05-25 17:26:49 +0300

    baseURL Update

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,21 +1,25 @@
-import axios from 'axios';
-import useAuthStore from '../store/useAuthStore';
+import axios from "axios";
+import useAuthStore from "../store/useAuthStore";
 
 // Configure Axios instance for backend calls
 export const api = axios.create({
-  baseURL: 'http://localhost:5002/api',
+  // baseURL: 'http://localhost:5002/api',
+  baseURL: "https://unroasted-hulk-designing.ngrok-free.dev/",
 });
 
 // Request Interceptor: Attach JWT Token if user is logged in
-api.interceptors.request.use((config) => {
-  const token = useAuthStore.getState().token;
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-});
+api.interceptors.request.use(
+  (config) => {
+    const token = useAuthStore.getState().token;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
 
 // Response Interceptor: Handle 401 Unauthorized (Expired Tokens)
 api.interceptors.response.use(
@@ -23,8 +27,8 @@ api.interceptors.response.use(
   (error) => {
     if (error.response?.status === 401) {
       useAuthStore.getState().logout();
-      window.location.href = '/login';
+      window.location.href = "/login";
     }
     return Promise.reject(error);
-  }
+  },
 );
