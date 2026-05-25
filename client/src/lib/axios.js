e63651c310c89cb8634a02d2f7a7b3dc53c4292a
Mohamed Behairy <134211075+mohamedbehairy@users.noreply.github.com>
2026-05-25 17:34:11 +0300

    baseURL/api update

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -4,7 +4,7 @@ import useAuthStore from "../store/useAuthStore";
 // Configure Axios instance for backend calls
 export const api = axios.create({
   // baseURL: 'http://localhost:5002/api',
-  baseURL: "https://unroasted-hulk-designing.ngrok-free.dev/",
+  baseURL: "https://unroasted-hulk-designing.ngrok-free.dev/api",
 });
 
 // Request Interceptor: Attach JWT Token if user is logged in
